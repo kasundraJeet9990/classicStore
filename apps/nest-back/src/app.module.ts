d02596d9fff0c4fc2c06e1450187e2ac4405860c
kasundraJeet <kasundrajeet29@gmail.com>
2025-05-04 00:05:08 +0530

    refactor: remove article module and update uuid generation
    The article module and its related files were removed as they are no longer needed in the codebase. Additionally, the uuid generation for the Article model in the Prisma schema was updated to use the default uuid() function instead of uuid(7).

--- apps/nest-back/src/app.module.ts
+++ apps/nest-back/src/app.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import { ArticleModule } from './article/article.module';
 import { GraphQLModule } from '@nestjs/graphql';
 import { MercuriusDriver, MercuriusDriverConfig } from '@nestjs/mercurius';
 import { join } from 'path';
@@ -10,7 +9,7 @@ import { join } from 'path';
     driver: MercuriusDriver,
     graphiql: true,
     autoSchemaFile: join(process.cwd(), '/graph/schema.gql'),
-  }), ArticleModule]
+  })]
 })
 
 export class AppModule {}
--- apps/nest-back/src/article/entities/index.ts
+++
@@ -1,6 +0,0 @@
-export * from './article-like.entity';
-export * from './article-read.entity';
-export * from './article-tag.entity';
-export * from './article-view.entity';
-export * from './article.entity';
-export * from './tagged-article.entity';
